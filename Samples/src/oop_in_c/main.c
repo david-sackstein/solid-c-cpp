2b90d55cd1d6028f6b2c2f698033ad6a09d3668d
David Sackstein <david.sackstein@fundguard.com>
2025-07-17 00:51:18 +0300

    more

--- Samples/src/oop_in_c/main.c
+++ Samples/src/oop_in_c/main.c
@@ -1,6 +1,8 @@
 #include "FileLogger.h"
+#include "ConsoleLogger.h"
 
 int main() {
+    // FileLogger usage
     FileLogger *flogger = create_file_logger();
     ILogger *logger = &flogger->base;
 
@@ -8,11 +10,21 @@ int main() {
         return 1;
     }
 
-    logger->log(logger, LOG_LEVEL_INFO, "Starting application");
-    logger->log(logger, LOG_LEVEL_DEBUG, "Debugging step X");
-    logger->log(logger, LOG_LEVEL_ERROR, "An error occurred");
+    logger->log(logger, LOG_LEVEL_INFO, "Starting application (file)");
+    logger->log(logger, LOG_LEVEL_DEBUG, "Debugging step X (file)");
+    logger->log(logger, LOG_LEVEL_ERROR, "An error occurred (file)");
 
     logger->close(logger);
+
+    // ConsoleLogger usage
+    ConsoleLogger *clogger = create_console_logger();
+    ILogger *console = &clogger->base;
+
+    console->open(console, NULL); // No-op
+    console->log(console, LOG_LEVEL_INFO, "Starting application (console)");
+    console->log(console, LOG_LEVEL_DEBUG, "Debugging step Y (console)");
+    console->log(console, LOG_LEVEL_ERROR, "An error occurred (console)");
+    console->close(console);
 
     return 0;
 }
